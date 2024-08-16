43f91bf1f8af8d2849a45f072728b55d8f6fa2c0
Chang Luo <33987852+luochang212@users.noreply.github.com>
2024-08-16 22:21:33 +0800

    update

--- util.py
+++ util.py
@@ -17,70 +17,6 @@ def gen_abspath(directory: str, rel_path: str) -> str:
     return os.path.join(abs_dir, rel_path)
 
 
-class Animator:
-    """在动画中绘制数据"""
-
-    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
-                 ylim=None, xscale='linear', yscale='linear',
-                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
-                 figsize=(3.5, 2.5)):
-
-        # 增量地绘制多条线
-        if legend is None:
-            legend = []
-        self.use_svg_display()
-        self.fig, self.axes = plt.subplots(nrows, ncols, figsize=figsize)
-        if nrows * ncols == 1:
-            self.axes = [self.axes, ]
-
-        # 使用lambda函数捕获参数
-        self.config_axes = lambda: self.set_axes(
-            self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
-        self.X, self.Y, self.fmts = None, None, fmts
-        
-    @staticmethod
-    def use_svg_display():
-        """Use the svg format to display a plot in Jupyter.
-
-        Defined in :numref:`sec_calculus`"""
-        from matplotlib_inline import backend_inline
-        backend_inline.set_matplotlib_formats('svg')
-    
-    @staticmethod
-    def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
-        """Set the axes for matplotlib.
-
-        Defined in :numref:`sec_calculus`"""
-        axes.set_xlabel(xlabel), axes.set_ylabel(ylabel)
-        axes.set_xscale(xscale), axes.set_yscale(yscale)
-        axes.set_xlim(xlim),     axes.set_ylim(ylim)
-        if legend:
-            axes.legend(legend)
-        axes.grid()
-
-    def add(self, x, y):
-        # 向图表中添加多个数据点
-        if not hasattr(y, "__len__"):
-            y = [y]
-        n = len(y)
-        if not hasattr(x, "__len__"):
-            x = [x] * n
-        if not self.X:
-            self.X = [[] for _ in range(n)]
-        if not self.Y:
-            self.Y = [[] for _ in range(n)]
-        for i, (a, b) in enumerate(zip(x, y)):
-            if a is not None and b is not None:
-                self.X[i].append(a)
-                self.Y[i].append(b)
-        self.axes[0].cla()
-        for x, y, fmt in zip(self.X, self.Y, self.fmts):
-            self.axes[0].plot(x, y, fmt)
-        self.config_axes()
-        display.display(self.fig)
-        display.clear_output(wait=True)
-
-
 class Accumulator:
     """在n个变量上累加"""
 
@@ -332,7 +268,6 @@ def random_split_cora_data(data, train_rate, val_rate):
     # 样本量
     num_train = int(N * train_rate)
     num_val = int(N * val_rate)
-    num_test = N - num_train - num_val
 
     # 掩码初始化
     train_mask = torch.zeros(N, dtype=torch.bool)
